ba355f583d95c5695b521cb188cf60b554d17951
Kevin Risqi <kevinrisqi22@gmail.com>
2019-10-21 00:34:56 +0700

    Add delay in Splash Screen

--- app/src/main/java/com/zenai/tiketonline/MainActivity.java
+++ app/src/main/java/com/zenai/tiketonline/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -15,13 +16,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ImageView splashlogo = findViewById(R.id.splashlogo);
 
-        splashlogo.setOnClickListener(new View.OnClickListener() {
+//        splashlogo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this,GetStartedAct.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
+            public void run() {
                 Intent intent = new Intent(MainActivity.this,GetStartedAct.class);
                 startActivity(intent);
                 finish();
             }
-        });
+        },1500);
     }
 }
