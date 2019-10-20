3f2284c8f978904b6c3dc790954e911d11d752eb
Kevin Risqi <kevinrisqi22@gmail.com>
2019-10-21 01:52:19 +0700

    Add ProfileFragment

--- app/src/main/java/com/zenai/tiketonline/GetStartedAct.java
+++ app/src/main/java/com/zenai/tiketonline/GetStartedAct.java
@@ -3,12 +3,16 @@ package com.zenai.tiketonline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class GetStartedAct extends AppCompatActivity {
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        name = findViewById(R.id.name);
     }
 }
