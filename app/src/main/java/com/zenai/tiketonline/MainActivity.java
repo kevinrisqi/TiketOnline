93c4c940a243115291ddf51f99de53d023b78b69
Kevin Risqi <kevinrisqi22@gmail.com>
2019-10-20 22:33:55 +0700

    Menambahkan method setOnClickListener pada SplashLogo

--- app/src/main/java/com/zenai/tiketonline/MainActivity.java
+++ app/src/main/java/com/zenai/tiketonline/MainActivity.java
@@ -2,7 +2,10 @@ package com.zenai.tiketonline;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView splashlogo = findViewById(R.id.splashlogo);
+
+        splashlogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,GetStartedAct.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
